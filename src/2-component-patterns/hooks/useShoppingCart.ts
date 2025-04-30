61806c9279492de4787f5590062cf551fac23b38
Gracia <graciamartin007@gmail.com>
2025-04-30 08:48:17 +0200

    control-props alternativo

--- src/2-component-patterns/hooks/useShoppingCart.ts
+++ src/2-component-patterns/hooks/useShoppingCart.ts
@@ -6,35 +6,20 @@ export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<{[key: string]: ProductInCart}>({});
 
     const onProductCountChange = ({count, product}: {count: number, product: Product}) => {
-        // console.log(count, product);
+        // console.log({count});
         setShoppingCart(oldShoppingCart => {
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0};
+            if(count === 0){
+                // delete ({...oldShoppingCart})[product.id]
+                const {[product.id]: toDelete, ...rest} = oldShoppingCart;
+                console.log({toDelete});
 
-            if(Math.max(productInCart.count + count, 0) > 0){
-                //productInCart.count = 20; --> Si existe que sea 20
-                productInCart.count += count;
-                return{
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
+                return rest;
             }
 
-            // Borrar el producto (articulo no existe o menor a 0)
-            const {[product.id]: toDelete, ...rest} = oldShoppingCart;
-            return rest;
-
-            // if(count === 0){
-            //     // delete ({...oldShoppingCart})[product.id]
-            //     const {[product.id]: toDelete, ...rest} = oldShoppingCart;
-            //     console.log({toDelete});
-
-            //     return rest;
-            // }
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [product.id]: {...product, count}
-            // }
+            return {
+                ...oldShoppingCart,
+                [product.id]: {...product, count}
+            }
         });
     }
 
