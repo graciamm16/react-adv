271810c5f9458709565b4f1055e8ad9dddcb2aea
Gracia <graciamartin007@gmail.com>
2025-05-07 09:57:16 +0200

    form-components-fields

---
+++ src/3-forms/pages/FormikAbstractation.tsx
@@ -0,0 +1,82 @@
+import '../styles/styles.css';
+import * as Yup from 'yup';
+import {Formik, Form} from 'formik';
+import { MyTextInput, MySelect, MyCheckBox } from '../components';
+
+
+export const FormikAbstractation = () => {
+    return (
+        <div>
+            <h1>Formik Abstractation</h1>
+
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    terms: false,
+                    jobType: ''
+                }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+                validationSchema={
+                    Yup.object({
+                        firstName: Yup.string()
+                                      .max(15, 'Debe tener 15 caracteres o menos')
+                                      .required('Requerido'),
+                        lastName: Yup.string()
+                                     .max(15, 'Debe tener 15 caracteres o menos')
+                                     .required('Requerido'),
+                        email: Yup.string()
+                                  .email('Email inválido')
+                                  .required('Requerido'),
+                        terms: Yup.boolean()
+                                  .oneOf([true], 'Debe aceptar las condiciones'),
+                        jobType: Yup.string()
+                                    .notOneOf(['it-junior'], 'Esta opción no está permitida')
+                                    .required('Requerido')
+                    })
+                }
+            >
+
+                {
+                    (formik) => (
+                        <Form>
+                            <MyTextInput 
+                                label='First Name' 
+                                name='firstName'
+                                placeholder='Gracia'
+                            />
+
+                            <MyTextInput 
+                                label='Last Name' 
+                                name='lastName'
+                                placeholder='Martin'
+                            />
+
+                            <MyTextInput 
+                                label='Email Address'
+                                name='email'
+                                placeholder='gracia@google.com'
+                                type='email'
+                            />
+
+                            <MySelect label='Job Type' name='jobType'>
+                                <option value="">Pick something</option>
+                                <option value="developer">Developer</option>
+                                <option value="designer">Designer</option>
+                                <option value="it-senior">IT Senior</option>
+                                <option value="it-junior">IT Junior</option>
+                            </MySelect>
+
+                            <MyCheckBox label='Terms & Conditions' name='terms'/>
+                            
+                            <button type='submit'>Submit</button>
+                        </Form>
+                    )
+                }   
+            </Formik>
+        </div>
+    )
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,45 +1,60 @@
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    NavLink
-  } from 'react-router-dom';
-   
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import logo from '../logo.svg';
-  
+import {FormikAbstractation, FormikBasicPage, FormikComponents, FormikYupPage, RegisterPage} from '../3-forms/pages';
+
+
 export const Navigation = () => {
   return (
-  <Router>
-    <div className="main-layout">
-      <nav>
-      <img src={ logo } alt="React Logo" />
-      <ul>
-      <li>
-      <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
-      </li>
-      <li>
-      <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
-      </li>
-      <li>
-      <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
-      </li>
-      </ul>
-      </nav>
-    
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-    <Switch>
-    <Route path="/about">
-    <h1>About</h1>
-    </Route>
-    <Route path="/users">
-    <h1>Users</h1>
-    </Route>
-    <Route path="/">
-    <h1>Home</h1>
-    </Route>
-    </Switch>
-    </div>
-  </Router>
+    <Router>
+      <div className="main-layout">
+        <nav>
+          <img src={ logo } alt="React Logo" />
+          
+          <ul>
+            <li>
+              <NavLink to="/register" activeClassName="nav-active" exact>Register Page</NavLink>
+            </li>
+            <li>
+              <NavLink to="/formik-basic" activeClassName="nav-active" exact>Formik Basic</NavLink>
+            </li>
+            <li>
+              <NavLink to="/formik-yup" activeClassName="nav-active" exact>Formik Yup</NavLink>
+            </li>
+            <li>
+              <NavLink to="/formik-components" activeClassName="nav-active" exact>Formik Components</NavLink>
+            </li>
+            <li>
+              <NavLink to="/formik-abstractation" activeClassName="nav-active" exact>Formik Abstractation</NavLink>
+            </li>
+            <li>
+              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+      
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/register">
+            <RegisterPage />
+          </Route>
+          <Route path="/formik-basic">
+            <FormikBasicPage />
+          </Route>
+          <Route path="/formik-yup">
+            <FormikYupPage />
+          </Route>
+          <Route path="/formik-components">
+            <FormikComponents />
+          </Route>
+          <Route path="/formik-abstractation">
+            <FormikAbstractation />
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
